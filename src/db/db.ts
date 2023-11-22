bfae16fec861059bc6b32c49f4e36e139c18dd0f
daviloops <91davidlopez@gmail.com>
2023-11-22 15:13:27 -0700

    Fix env file

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,7 +10,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NEXT_PUBLIC_NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
 
